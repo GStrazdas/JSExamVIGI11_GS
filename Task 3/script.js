bc700f22214e589b4b6ea050ff294a45e47ab255
GStrazdas <ged.strazdas@gmail.com>
2022-01-19 19:15:23 +0200

    Task3 completed

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,3 +11,44 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+async function getList(link) {
+    try {
+        const response = await fetch(link);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+document.getElementById("btn").addEventListener("click", async () => {
+    const data = await getList(ENDPOINT);
+    showList(data);
+});
+
+function showList(data) {
+    if(document.getElementById("message"))
+        document.getElementById("message").remove();
+    if(!document.getElementById("result")) {
+        const output = document.getElementById("output");
+        const result = document.createElement("div");
+        result.id = "result";
+        output.append(result);
+        data.forEach(element => {
+            const avatar = document.createElement("img");
+            const login = document.createElement("h1");
+            const avatar_url = document.createElement("h3");
+            avatar.src = element.avatar_url;
+            avatar.style.width = "100px";
+            avatar.style.borderRadius = "50%";
+            login.style.color = "black";
+            login.textContent = element.login
+            avatar_url.textContent = element.avatar_url;
+            avatar_url.style.marginBottom = "3rem";
+            result.append(avatar);
+            result.append(login);
+            result.append(avatar_url);
+        });
+    }
+}
