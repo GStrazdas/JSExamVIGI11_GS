4d8dc09c3284d73ea6fb6eacb409c91c72583611
GStrazdas <ged.strazdas@gmail.com>
2022-01-20 21:26:19 +0200

    Task6 completed

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,14 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+function getUserAverageAge(array) {
+  return array.reduce((pre, val) => pre = pre + val.age, 0)/array.length;
+}
+
+function getUsersNames(array) {
+  return array.map((element) => element.name);
+}
+
+console.log(getUserAverageAge(users));
+console.log(getUsersNames(users));
